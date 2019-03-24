695f798259affedc28f895ecd2110a8ef9a3841b
tamlt2704 <tamlt2704@gmail.com>
2019-03-24 12:54:10 +0800

    change config to test push to server

--- vcb.py
+++ vcb.py
@@ -1,5 +1,11 @@
 import urllib3
+import xml.etree.ElementTree as ET
+
 
 http = urllib3.PoolManager()
 r = http.request('GET', "https://www.vietcombank.com.vn/ExchangeRates/ExrateXML.aspx")
-print(r.data)
+
+tree = ET.fromstring(r.data)
+root = tree.getroot()
+for child in root:
+    print(child.tag, child.text)
