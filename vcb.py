bb91c3a3f2408e2ef2df36d27b660c190a0396eb
tamlt2704 <tamlt2704@gmail.com>
2019-03-24 13:20:11 +0800

    update script to populate data

--- vcb.py
+++ vcb.py
@@ -1,11 +1,33 @@
 import urllib3
 import xml.etree.ElementTree as ET
+import pandas as pd
+import os
 
+# create data folder if not exist
+data_folder = 'data'
+if not os.path.exists(data_folder):
+    os.mkdir(data_folder)
 
+# get data
 http = urllib3.PoolManager()
 r = http.request('GET', "https://www.vietcombank.com.vn/ExchangeRates/ExrateXML.aspx")
 
-tree = ET.fromstring(r.data)
-root = tree.getroot()
+# parse xml
+root = ET.fromstring(r.data)
+date = None
+arr = []
 for child in root:
-    print(child.tag, child.text)
+    tag, attrib = child.tag, child.attrib
+    if tag == 'DateTime':
+        date = child.text
+    elif tag == 'Exrate':
+        arr.append(child.attrib)
+        
+# convert to dataframe and save to file
+df = pd.DataFrame(arr)
+df['date'] = date
+df['date'] = pd.to_datetime(df['date'])
+dt = str(df['date'].iloc[0])[:10]
+filepath = "{}/{}.csv".format(data_folder, dt)
+df.to_csv(filepath)
+print("save", df.shape[0], "rows to ", data_folder)
